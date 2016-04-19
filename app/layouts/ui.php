78e6a38152969df372dd313c4254ae5cd5bde154
Celine Rufener <celine.may@gmail.com>
2016-04-19 13:08:15 +1200

    UI -> Responsive layout

--- app/layouts/ui.php
+++ app/layouts/ui.php
@@ -1,4 +1,4 @@
-<div class="ui">
+<div class="ui small-hidden">
   <h1 class="logo">
     <span class="hidden">Oddboy</span>
     <svg class="shape-logo">
@@ -18,5 +18,26 @@
 
   <a href="<?php echo PATH; ?>talk-to-us" class="nav-link rhs">
     Talk to us
+  </a>
+</div>
+
+<div class="ui small-only">
+  <div class="ui-header bg-color secondary-dark">
+    <a href="<?php echo PATH; ?>" class="back-link">
+      <svg class="shape-arrow">
+        <use xlink:href="#shape-arrow"></use>
+      </svg>
+    </a>
+
+    <h2 class="ui-title title wwd">What we do</h2>
+    <h2 class="ui-title title ttu">Talk to us</h2>
+  </div>
+
+  <a href="<?php echo PATH; ?>talk-to-us" class="nav-link-small top title">
+    Talk to us
+  </a>
+
+  <a href="<?php echo PATH; ?>what-we-do" class="nav-link-small bottom title">
+    What we do
   </a>
 </div>
