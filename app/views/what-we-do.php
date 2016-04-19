93ee1594affb71881106615d085825677c476be1
Celine Rufener <celine.may@gmail.com>
2016-04-19 15:15:41 +1200

    What we do -> Responsive layout

--- app/views/what-we-do.php
+++ app/views/what-we-do.php
@@ -5,7 +5,7 @@
       <span class="text-color primary">Odd<span class="flipped">b</span>oy</span> is an experience design studio<button class="text-color primary do-show-complement">*</button>
     </h1>
     <span class="title-separator bg-color accent do-complement-push"></span>
-    <p class="title-complement copy">
+    <p class="title-complement copy touch-hidden">
       <span class="text-color primary">*</span>That’s just a roundabout way of saying we create digital stuff that you’ll remember.
     </p>
     <p class="lead do-complement-push">
@@ -23,7 +23,7 @@
   <?php include('layouts/grid.php'); ?>
   <div class="section-content">
     <article class="service rhs" data-service="game-design">
-      <p class="service-highlights">
+      <p class="service-highlights large-only">
         Beautiful
         <span class="highlights-separator"></span>
         Engaging
@@ -33,14 +33,14 @@
         <img src="<?php echo IMAGES_PATH; ?>services/game-design.jpg" alt="Game Design">
       </div>
       <div class="service-description">
-        <h2 class="service-title title">Game&nbsp;Design<span class="text-color accent">.</span></h2>
+        <h2 class="service-title title">Game Design<span class="text-color accent">.</span></h2>
         <div class="service-icon">
           <svg class="shape-gamepad">
             <use xlink:href="#shape-gamepad"></use>
           </svg>
         </div>
         <p class="service-lead lead">
-          The fun of shooting someone in the face, without any of the consequences.
+          The fun of shooting someone in the face, without any of the&nbsp;consequences.
         </p>
         <p class="service-copy copy">
           Video games engage, teach and inspire us in ways that the real world cannot. Through emotionally driven stories and rewarding problem solving, we feel uniquley connected to games. Our passion is understanding this connection and using it to create beautifully immersive games. Or an FPS.
@@ -57,7 +57,7 @@
     </article>
 
     <article class="service lhs" data-service="virtual-reality">
-      <p class="service-highlights">
+      <p class="service-highlights large-only">
         Immersive
         <span class="highlights-separator"></span>
         Magical
@@ -67,7 +67,7 @@
         <img src="<?php echo IMAGES_PATH; ?>services/virtual-reality.png" alt="Virtual Reality">
       </div>
       <div class="service-description">
-        <h2 class="service-title title">Virtual&nbsp;Reality<span class="text-color accent">.</span></h2>
+        <h2 class="service-title title">Virtual Reality<span class="text-color accent">.</span></h2>
         <div class="service-icon">
           <svg class="shape-vr-gear">
             <use xlink:href="#shape-vr-gear"></use>
@@ -83,7 +83,7 @@
     </article>
 
     <article class="service rhs" data-service="digital-products">
-      <p class="service-highlights">
+      <p class="service-highlights large-only">
         Intelligent
         <span class="highlights-separator"></span>
         Intuitive
@@ -93,14 +93,14 @@
         <img src="<?php echo IMAGES_PATH; ?>services/digital-products.png" alt="Digital Products">
       </div>
       <div class="service-description">
-        <h2 class="service-title title">Digital&nbsp;Products<span class="text-color accent">.</span></h2>
+        <h2 class="service-title title">Digital Products<span class="text-color accent">.</span></h2>
         <div class="service-icon">
           <svg class="shape-atom">
             <use xlink:href="#shape-atom"></use>
           </svg>
         </div>
         <p class="service-lead lead">
-          If it seems confusing or complex it’s because it is... But it shouldn’t be.
+          If it seems confusing or complex it’s because it is... But it shouldn’t&nbsp;be.
         </p>
         <p class="service-copy copy">
           The digital realm is everywhere, your phone, car, TV, even your fridge. Everything is becoming connected. Products and digital interfaces are becoming one in the same. We want to  craft intuitive solutions that make life that little easier and create moments of joy.
@@ -117,7 +117,7 @@
     <div class="work-wrapper">
       <span class="floating-letter title text-color accent">B</span>
 
-      <h2 class="work-wrapper-title title">Some of our work<span class="text-color primary">.</span></h2>
+      <h2 class="work-wrapper-title title">Some of our&nbsp;work<span class="text-color primary">.</span></h2>
 
       <article class="work" data-work="camp-hope-falls">
         <div class="work-content">
@@ -169,27 +169,27 @@
         </div>
       </article>
     </div>
-
-    <footer class="footer bg-color accent">
-      <div class="footer-cell lhs">
-        <a href="https://www.behance.net/oddboy" target="_blank" class="footer-social">
-          <svg class="shape-behance">
-            <use xlink:href="#shape-behance"></use>
-          </svg>
-        </a>
-
-        <a href="#todo" target="_blank" class="footer-social">
-          <svg class="shape-facebook">
-            <use xlink:href="#shape-facebook"></use>
-          </svg>
-        </a>
-      </div>
-
-      <div class="footer-cell">
-        <p class="copyright">
-          &copy; Oddboy <?php echo date('Y'); ?>
-        </p>
-      </div>
-    </footer>
   </div>
 </section>
+
+<footer class="footer bg-color accent">
+  <div class="footer-cell lhs">
+    <a href="https://www.behance.net/oddboy" target="_blank" class="footer-social">
+      <svg class="shape-behance">
+        <use xlink:href="#shape-behance"></use>
+      </svg>
+    </a>
+
+    <a href="#todo" target="_blank" class="footer-social">
+      <svg class="shape-facebook">
+        <use xlink:href="#shape-facebook"></use>
+      </svg>
+    </a>
+  </div>
+
+  <div class="footer-cell">
+    <p class="copyright">
+      &copy; Oddboy <?php echo date('Y'); ?>
+    </p>
+  </div>
+</footer>
