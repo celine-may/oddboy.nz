863e6ca6daecd68342f9ab908b5edacb79fd6932
Celine Rufener <celine.may@gmail.com>
2016-04-23 12:22:01 +1200

    Dist config

---
+++ dist/config.php
@@ -0,0 +1,25 @@
+<?php
+
+// Global variables + SMTP init
+define('PATH', '/staging/');
+define('EMAIL', 'ben@oddboy.nz');
+define('IMAGES_PATH', PATH . 'assets/images/');
+define('VIDEOS_PATH', PATH . 'assets/videos/');
+
+date_default_timezone_set('Pacific/Auckland');
+
+// Errors display
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+// Assets
+$assets = array();
+
+$js_path = PATH.'assets/javascripts/';
+$css_path = PATH.'assets/stylesheets/';
+$assets['javascripts'] = array(
+  $js_path . 'application.min.js'
+);
+$assets['stylesheets'] = array(
+  $css_path . 'application.min.css',
+);
