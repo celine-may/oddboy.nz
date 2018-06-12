36cb769e30fc96adea47169dde2e844202566f27
Celine Rufener <celine.may@gmail.com>
2018-06-12 20:39:10 +1200

    Move scroll CTA to home

--- app/views/home.php
+++ app/views/home.php
@@ -1,1 +1,16 @@
-<section class="view" data-view="home"></section>
+<section class="view" data-view="home">
+  <header class="header">
+    <button class="scroll-cta do-slide-up do-scroll-down" data-view="projects">
+      <span class="scroll-circle">
+        <span class="scroll-bg"></span>
+      </span>
+      <svg class="shape-scroll-cta">
+        <use xlink:href="#shape-scroll-cta"></use>
+      </svg>
+    </button>
+  </header>
+
+  <section class="section projects-wrapper bg-color secondary">
+    <?php include('layouts/grid.php'); ?>
+  </section>
+</section>
