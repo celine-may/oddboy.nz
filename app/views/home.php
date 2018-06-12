0934041ebb0c7b797f3ccf2fdf092e88bb98d64f
Celine Rufener <celine.may@gmail.com>
2018-06-12 21:04:48 +1200

    Projects -> add titles

--- app/views/home.php
+++ app/views/home.php
@@ -12,5 +12,34 @@
 
   <section class="section projects-wrapper bg-color secondary">
     <?php include('layouts/grid.php'); ?>
+    <div class="title-wrapper">
+      <h2 class="title">Oddboy<br/>projects<span class="text-color accent">.</span></h2>
+      <div class="title-icons">
+        <svg class="shape shape-gamepad">
+          <use xlink:href="#shape-gamepad"></use>
+        </svg>
+        <svg class="shape shape-vr-gear">
+          <use xlink:href="#shape-vr-gear"></use>
+        </svg>
+        <svg class="shape shape-atom">
+          <use xlink:href="#shape-atom"></use>
+        </svg>
+      </div>
+    </div>
+
+    <div class="title-wrapper">
+      <h2 class="title">Projects<br/>for clients<span class="text-color accent">.</span></h2>
+      <div class="title-icons">
+        <svg class="shape shape-gamepad">
+          <use xlink:href="#shape-gamepad"></use>
+        </svg>
+        <svg class="shape shape-vr-gear">
+          <use xlink:href="#shape-vr-gear"></use>
+        </svg>
+        <svg class="shape shape-atom">
+          <use xlink:href="#shape-atom"></use>
+        </svg>
+      </div>
+    </div>
   </section>
 </section>
