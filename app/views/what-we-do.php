e139fa6e2b3227d79fd97aa6faac65b49fae6376
Celine Rufener <celine.may@gmail.com>
2018-06-14 21:01:22 +1200

    Create a reusable footer layout

--- app/views/what-we-do.php
+++ app/views/what-we-do.php
@@ -227,25 +227,5 @@
     </div>
   </div>
 
-  <footer class="footer bg-color accent">
-    <div class="footer-cell lhs">
-      <a href="https://www.behance.net/oddboy" target="_blank" class="footer-social">
-        <svg class="shape-behance">
-          <use xlink:href="#shape-behance"></use>
-        </svg>
-      </a>
-
-      <a href="https://www.facebook.com/oddboynz" target="_blank" class="footer-social">
-        <svg class="shape-facebook">
-          <use xlink:href="#shape-facebook"></use>
-        </svg>
-      </a>
-    </div>
-
-    <div class="footer-cell">
-      <p class="copyright">
-        &copy; Oddboy <?php echo date('Y'); ?>
-      </p>
-    </div>
-  </footer>
+  <?php include('layouts/footer.php') ?>
 </section>
